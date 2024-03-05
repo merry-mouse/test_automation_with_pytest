4ddac2f2b194875468e7f770a637085b664d4615
merry-mouse <potekh.anastasia@gmail.com>
2024-03-05 16:23:09 +0100

    create more tests for post companies

--- api/django_udemy/companies/tests/test_api.py
+++ api/django_udemy/companies/tests/test_api.py
@@ -41,3 +41,45 @@ class TestPostCompanies(BasicCompanyApiTestCase):
         self.assertEqual(
             json.loads(response.content), {"name": ["This field is required."]}
         )
+
+    def test_create_existing_company_fail(self):
+        Company.objects.create(name="apple")
+        response = self.client.post(
+            path=self.companies_url,
+            data={"name": "apple"},
+        )
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(
+            json.loads(response.content),
+            {"name": ["company with this name already exists."]},
+        )
+
+    def test_create_company_with_only_name_other_fields_default(self):
+        response = self.client.post(
+            path=self.companies_url,
+            data={"name": "test_company_name"},
+        )
+        self.assertEqual(response.status_code, 201)
+        response_content = json.loads(response.content)
+        self.assertEqual(response_content.get("name"), "test_company_name")
+        self.assertEqual(response_content.get("status"), "Hiring")
+        self.assertEqual(response_content.get("application_link"), "")
+        self.assertEqual(response_content.get("notes"), "")
+
+    def test_create_company_with_layoffs_success(self):
+        response = self.client.post(
+            path=self.companies_url,
+            data={"name": "test_company_name", "status": "Layoffs"},
+        )
+        self.assertEqual(response.status_code, 201)
+        response_content = json.loads(response.content)
+        self.assertEqual(response_content.get("status"), "Layoffs")
+
+    def test_create_company_with_wrong_status_fail(self):
+        response = self.client.post(
+            path=self.companies_url,
+            data={"name": "test_company_name", "status": "wrong_status"},
+        )
+        self.assertEqual(response.status_code, 400)
+        self.assertIn("wrong_status", str(response.content))
+        self.assertIn("is not a valid choice", str(response.content))
