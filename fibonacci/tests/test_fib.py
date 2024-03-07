77b9c5d43101539e8c88cb7f8d98201ef4f63b52
merry-mouse <potekh.anastasia@gmail.com>
2024-03-07 10:56:48 +0100

    trying out lru_chache

--- fibonacci/tests/test_fib.py
+++ fibonacci/tests/test_fib.py
@@ -1,8 +1,11 @@
+from functools import lru_cache
+
 import pytest
 
 from fibonacci.naive import fibonacci_naive
 
 
+@lru_cache(maxsize=256)
 @pytest.mark.parametrize("n,expected", [(0, 0), (1, 1), (2, 1), (20, 6765)])
 def test_naive(n: int, expected: int):
     res = fibonacci_naive(n=n)
