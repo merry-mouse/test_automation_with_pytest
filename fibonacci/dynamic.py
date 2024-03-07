941089c02b408bb34b27d349fa911544eb9b4e7c
merry-mouse <potekh.anastasia@gmail.com>
2024-03-07 13:58:36 +0100

    create secod dynamic fibonacci func

--- fibonacci/dynamic.py
+++ fibonacci/dynamic.py
@@ -3,3 +3,13 @@ def fibonacci_dynamic(n: int):
     for i in range(1, n + 1):
         fib_list.append(fib_list[i] + fib_list[i - 1])
     return fib_list[n]
+
+
+def fibonaci_dynamic_v2(n: int):
+    fib_1, fib_2 = 0, 1
+
+    for i in range(1, n + 1):
+        fi = fib_1 + fib_2
+        fib_1, fib_2 = fib_2, fi
+
+    return fib_1
--- fibonacci/tests/test_fib.py
+++ fibonacci/tests/test_fib.py
@@ -2,13 +2,15 @@ from typing import Callable
 
 import pytest
 
-from fibonacci.dynamic import fibonacci_dynamic
+from fibonacci.dynamic import fibonacci_dynamic, fibonaci_dynamic_v2
 from fibonacci.naive import fibonacci_naive
 from fixtures import time_tracker
 
 
 @pytest.mark.parametrize("n,expected", [(0, 0), (1, 1), (2, 1), (20, 6765)])
-@pytest.mark.parametrize("fib_func", [fibonacci_naive, fibonacci_dynamic])
+@pytest.mark.parametrize(
+    "fib_func", [fibonacci_naive, fibonacci_dynamic, fibonaci_dynamic_v2]
+)
 def test_naive(
     time_tracker,
     fib_func: Callable[[int], int],
