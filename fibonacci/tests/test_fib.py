fabf06f9989e238cbd4190f194381480ee846d23
merry-mouse <potekh.anastasia@gmail.com>
2024-03-07 11:52:31 +0100

    modify fibonacci func and tests

--- fibonacci/tests/test_fib.py
+++ fibonacci/tests/test_fib.py
@@ -1,12 +1,18 @@
-from functools import lru_cache
+from typing import Callable
 
 import pytest
 
 from fibonacci.naive import fibonacci_naive
+from fixtures import time_tracker
 
 
-@lru_cache(maxsize=256)
 @pytest.mark.parametrize("n,expected", [(0, 0), (1, 1), (2, 1), (20, 6765)])
-def test_naive(n: int, expected: int):
-    res = fibonacci_naive(n=n)
+@pytest.mark.parametrize("fib_func", [fibonacci_naive])
+def test_naive(
+    time_tracker,
+    fib_func: Callable[[int], int],
+    n: int,
+    expected: int,
+):
+    res = fib_func(n)
     assert res == expected
